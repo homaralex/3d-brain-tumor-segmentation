8cb5305424ae540ab5499d0d61686cb36a888f9a
alexander.rakowski <alexander.rakowski@hpi.de>
2021-06-11 14:23:00 +0200

    WIP: wandb logging

--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 import os
 import tensorflow as tf
 import numpy as np
+import wandb
 from tqdm import tqdm
 
 from args import TrainArgParser
@@ -11,6 +12,7 @@ from model import Model
 
 def prepare_dataset(loc, batch_size, prepro_size, crop_size, out_ch, shuffle=True, data_format='channels_last'):
     """Returns a BatchDataset object containing loaded data."""
+
     def parse_example(example_proto):
         # Parse examples.
         parsed = tf.io.parse_single_example(example_proto, example_desc)
@@ -26,20 +28,20 @@ def prepare_dataset(loc, batch_size, prepro_size, crop_size, out_ch, shuffle=Tru
 
         # Create random crop.
         xy = tf.concat([x, y], axis=-1)
-        xy = tf.image.random_crop(xy, crop_size + [c+1])
+        xy = tf.image.random_crop(xy, crop_size + [c + 1])
 
         # Randomly flip across each axis.
         for axis in (0, 1, 2):
             xy = tf.cond(
-                    tf.random.uniform(()) > 0.5,
-                    lambda: tf.reverse(xy, axis=[axis]),
-                    lambda: xy)
+                tf.random.uniform(()) > 0.5,
+                lambda: tf.reverse(xy, axis=[axis]),
+                lambda: xy)
 
         x, y = tf.split(xy, [c, 1], axis=-1)
-        
+
         # Convert labels into one-hot encodings.
         y = tf.squeeze(tf.cast(y, tf.int32), axis=-1)
-        y = tf.one_hot(y, out_ch+1, axis=-1, dtype=tf.float32)
+        y = tf.one_hot(y, out_ch + 1, axis=-1, dtype=tf.float32)
         _, y = tf.split(y, [1, out_ch], axis=-1)
 
         if data_format == 'channels_first':
@@ -54,7 +56,8 @@ def prepare_dataset(loc, batch_size, prepro_size, crop_size, out_ch, shuffle=Tru
         'x': tf.io.FixedLenFeature([h * w * d * c], tf.float32),
         'y': tf.io.FixedLenFeature([h * w * d * 1], tf.float32)}
 
-    files = [os.path.join(loc, f) for f in os.listdir(loc)]
+    # TODO remove slice
+    files = [os.path.join(loc, f) for f in os.listdir(loc)][:2]
     dataset = tf.data.TFRecordDataset(files)
 
     # Shuffle for training set, else no shuffle for validation set.
@@ -62,30 +65,30 @@ def prepare_dataset(loc, batch_size, prepro_size, crop_size, out_ch, shuffle=Tru
         dataset = dataset.shuffle(buffer_size=len(files))
 
     dataset = (dataset.map(map_func=parse_example, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-                      .batch(batch_size=batch_size)
-                      .prefetch(buffer_size=tf.data.experimental.AUTOTUNE))
-    
+               .batch(batch_size=batch_size)
+               .prefetch(buffer_size=tf.data.experimental.AUTOTUNE))
+
     return dataset, len(files)
 
 
 def train(args):
     # Load data.
     train_data, n_train = prepare_dataset(
-                                args.train_loc,
-                                args.batch_size,
-                                args.prepro_size,
-                                args.crop_size,
-                                args.model_args['out_ch'],
-                                shuffle=True,
-                                data_format=args.data_format)
+        args.train_loc,
+        args.batch_size,
+        args.prepro_size,
+        args.crop_size,
+        args.model_args['out_ch'],
+        shuffle=True,
+        data_format=args.data_format)
     val_data, n_val = prepare_dataset(
-                                args.val_loc,
-                                args.batch_size,
-                                args.prepro_size,
-                                args.crop_size,
-                                args.model_args['out_ch'],
-                                shuffle=False,
-                                data_format=args.data_format)
+        args.val_loc,
+        args.batch_size,
+        args.prepro_size,
+        args.crop_size,
+        args.model_args['out_ch'],
+        shuffle=False,
+        data_format=args.data_format)
     print('{} training examples.'.format(n_train), flush=True)
     print('{} validation examples.'.format(n_val), flush=True)
 
@@ -93,13 +96,14 @@ def train(args):
     in_ch = args.prepro_size[-1]
     model = Model(**args.model_args)
     _ = model(tf.zeros(shape=[1] + args.crop_size + [in_ch] if args.data_format == 'channels_last' \
-                                    else [1, in_ch] + args.crop_size))
-    
+        else [1, in_ch] + args.crop_size))
+    model.summary()
+
     # Load weights if specified.
     if args.load_folder:
         model.load_weights(os.path.join(args.load_folder, 'chkpt.hdf5'))
     n_params = tf.reduce_sum([tf.reduce_prod(var.shape) for var in model.trainable_variables])
-    print('Total number of parameters: {}'.format(n_params), flush=True)
+    print('Total number of parameters: {} m'.format(int(n_params) // 1e6), flush=True)
 
     # Initialize loss and metrics.
     optimizer = ScheduledOptim(learning_rate=args.lr)
@@ -112,7 +116,7 @@ def train(args):
     val_loss = tf.keras.metrics.Mean(name='val_loss')
     val_macro_dice = tf.keras.metrics.Mean(name='val_macro_dice')
     val_micro_dice = tf.keras.metrics.Mean(name='val_micro_dice')
-    
+
     # Set up logging.
     if args.save_folder:
         with open(os.path.join(args.save_folder, 'train.log'), 'w') as f:
@@ -125,6 +129,11 @@ def train(args):
                                'val_macro_dice',
                                'val_micro_dice'])
             f.write(header + '\n')
+
+    # TODO
+    wandb_project = 'test'
+    if wandb_project is not None:
+        wandb.init(project=wandb_project, config=vars(args))
 
     best_val_dice = 0.0
     patience = 0
@@ -141,7 +150,7 @@ def train(args):
                 # Forward and loss.
                 with tf.GradientTape() as tape:
                     y_pred, y_vae, z_mean, z_logvar = model(x, training=True, inference=False)
-                    
+
                     loss = loss_fn(x, y, y_pred, y_vae, z_mean, z_logvar)
                     loss += tf.reduce_sum(model.losses)
 
@@ -157,9 +166,9 @@ def train(args):
                 train_micro_dice.update_state(micro_dice)
 
             print('Training. Loss: {l: .4f}, Macro Dice: {d1: 1.4f}, Micro Dice: {d2: 1.4f}.'
-                        .format(l=train_loss.result(),
-                                d1=train_macro_dice.result(),
-                                d2=train_micro_dice.result()), flush=True)
+                  .format(l=train_loss.result(),
+                          d1=train_macro_dice.result(),
+                          d2=train_micro_dice.result()), flush=True)
 
             # Validation epoch.
             for x, y in tqdm(val_data, total=n_val, desc='Validation    '):
@@ -176,9 +185,9 @@ def train(args):
                 val_micro_dice.update_state(micro_dice)
 
             print('Validation. Loss: {l: .4f}, Macro Dice: {d1: 1.4f}, Micro Dice: {d2: 1.4f}.'
-                        .format(l=val_loss.result(),
-                                d1=val_macro_dice.result(),
-                                d2=val_micro_dice.result()), flush=True)
+                  .format(l=val_loss.result(),
+                          d1=val_macro_dice.result(),
+                          d2=val_micro_dice.result()), flush=True)
 
         # Write logs.
         if args.save_folder:
@@ -192,6 +201,17 @@ def train(args):
                                   str(val_macro_dice.result().numpy()),
                                   str(val_micro_dice.result().numpy())])
                 f.write(entry + '\n')
+        if wandb_project is not None:
+            wandb.log({
+                'epoch': epoch,
+                'lr': optimizer.learning_rate.numpy(),
+                'train_loss': train_loss.result().numpy(),
+                'train_macro_dice': train_macro_dice.result().numpy(),
+                'train_micro_dice': train_micro_dice.result().numpy(),
+                'val_loss': val_loss.result().numpy(),
+                'val_macro_dice': val_macro_dice.result().numpy(),
+                'val_micro_dice': val_micro_dice.result().numpy(),
+            })
 
         # Checkpoint and patience.
         if val_macro_dice.result() > best_val_dice:
@@ -202,7 +222,7 @@ def train(args):
             print('Saved model weights.', flush=True)
         elif patience == args.patience:
             print('Validation dice has not improved in {} epochs. Stopped training.'
-                    .format(args.patience), flush=True)
+                  .format(args.patience), flush=True)
             return
         else:
             patience += 1
